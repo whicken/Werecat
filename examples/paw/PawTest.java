8d06198aa633bbe8eb50a893c9377c25c9d1fc59
Wendell Hicken <whicken@gmail.com>
2011-06-28 20:47:53 -0700

    Sample code

---
+++ examples/paw/PawTest.java
@@ -0,0 +1,28 @@
+import com.whicken.werecat.paw.*;
+import java.util.*;
+
+public class PawTest {
+    public static class Artist {
+	String name;
+	int age;
+	public Artist(String name, int age) {
+	    this.name = name;
+	    this.age = age;
+	}
+	public String getName() { return name; }
+	public int getAge() { return age; }
+    };
+    public static void main(String[] args) {
+	List<Artist> artists = new ArrayList<Artist>();
+	artists.add(new Artist("Bob", 20));
+	artists.add(new Artist("Jill", 21));
+
+	PawParser<Artist> parser = new PawParser(Artist.class);
+	PawExpression<Artist> expr = parser.parse("age == 20");
+	for (Artist a : artists) {
+	    Object o = expr.getValue(a);
+	    System.out.println(a.name+": "+o);
+	}
+	
+    }
+}
