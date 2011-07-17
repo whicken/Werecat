2a4a4958484d3c9d7ee13fcc9475ce029bc6d494
Wendell Hicken <whicken@gmail.com>
2011-07-17 10:10:41 -0700

    Made RuleContext an interface (duh)

--- src/com/whicken/werecat/RuleContext.java
+++ src/com/whicken/werecat/RuleContext.java
@@ -4,5 +4,5 @@ package com.whicken.werecat;
  * Used to evaluate a path through the RuleEngine.  Subclass and add any
  * data necessary to evaluate rules.
  */
-public class RuleContext {
+public interface RuleContext {
 }
--- src/com/whicken/werecat/paw/PawContext.java
+++ src/com/whicken/werecat/paw/PawContext.java
@@ -2,7 +2,7 @@ package com.whicken.werecat.paw;
 
 import com.whicken.werecat.RuleContext;
 
-class PawContext<T> extends RuleContext {
+class PawContext<T> implements RuleContext {
     T object;
     PawContext(T object) {
 	this.object = object;
